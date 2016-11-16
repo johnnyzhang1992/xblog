3665a20efec71bc79602d4252a7ebc7293379a11
johnnyzhang <1113638674@qq.com>
2016-11-16 22:54:56 +0800

    增加用户访问统计表

---
+++ database/migrations/2016_11_16_181231_create_visitor_trackings_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVisitorTrackingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visitor_tracking', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username');
+            $table->string('email');
+            $table->string('from_url');
+            $table->string('to_url');
+            $table->string('to_url');
+            $table->integer('session_id')->index()->nullable(true);
+            $table->string('ip');
+            $table->string('device');
+            $table->string('language');
+            $table->string('os');
+            $table->string('os_version');
+            $table->string('browser');
+            $table->string('browser_version');
+            $table->string('robot');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visitor_tracking');
+    }
+}
