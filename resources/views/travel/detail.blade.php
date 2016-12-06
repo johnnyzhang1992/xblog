1e123d4046c52943ed3c7ed3953ebcaa188e3011
johnnyzhang <1113638674@qq.com>
2016-12-06 23:26:20 +0800

    poi详情页模板更新

--- resources/views/travel/detail.blade.php
+++ resources/views/travel/detail.blade.php
@@ -1,60 +1,78 @@
 @extends('layouts.app')
 @section('title','旅行日记')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('/css/travel/main.css') }}">
+    <link rel="stylesheet" href="http://api.map.baidu.com/library/SearchInfoWindow/1.5/src/SearchInfoWindow_min.css" />
+    <style>
+        .poi-detail{
+            background: #fff;
+            padding: 20px 15px;
+            margin-bottom: 15px;
+            border-radius:5px ;
+            position: relative;
+            box-shadow: 1px 1px 3px #666;
+            -moz-box-shadow: 1px 1px 3px #666;
+            -webkit-box-shadow: 1px 1px 3px #666;
+        }
+        .poi-detail h2{
+            margin-top: 5px;
+            margin-bottom: 10px;
+        }
+        .poi-detail h4 span{
+            margin-top: 5px;
+            border-left: 3px solid #43ce7b;
+            padding-left: 10px;
+        }
+        .poi-icons{
+            margin-right: 5px;
+        }
+    </style>
+@endsection
 @section('content')
 
     <div class="container" >
-        <div class="col-md-offset-1 col-md-10 col-xs-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <p class="panel-title">{{@$poi->poi_name}}</p>
+        <div class="col-md-12 col-xs-12">
+            <div class="col-xs-12">
+                <ol class="breadcrumb">
+                    <li><a href="{{ url('/travel') }}">Travel</a></li>
+                    <li>{{@$poi->tag}}</li>
+                    <li class="active">{{@$poi->poi_name}}</li>
+                    @if(Auth::check())
+                        <li class="pull-right"><a href="{{ url('admin/poi/edit/'.$poi->id) }}" ><i class="fa fa-pencil"></i> 编辑</a></li>
+                    @else
+                    @endif
+                </ol>
+                <div class="poi-detail clearfix">
+                    <h2>{{ @$poi->poi_name }}</h2>
+                    <p>
+                        <span class="fa fa-map-marker poi-icons"></span>
+                        {{@$poi->address}}
+                    </p>
                 </div>
-                <div class="panel-body">
-                    <!--标签（Tag）-->
-                    <div class="col-md-12 col-sm-12">
-                        <p class="col-sm-2 ">标签(Tag)</p>
-                        <div class="col-sm-10">
-                            <p>{{@$poi->tag}}</p>
-                        </div>
+                <div class="poi-detail clearfix">
+                    <h4><span>基本信息</span></h4>
+                    <div class="col-md-6 col-xs-12">
+                        <table class="table table-striped table-hover">
+                            <tr>
+                                <td >经度(Lat)</td>
+                                <td>{{@$poi->lat}}</td>
+                            </tr>
+                            <tr>
+                                <td>纬度(Lng)</td>
+                                <td>{{@$poi->lng}}</td>
+                            </tr>
+                        </table>
                     </div>
-                    <!--经纬度（LatLng）-->
-                    <div class="col-md-12 col-sm-12">
-                        <div class="col-sm-6" style="padding-left: 0">
-                            <p class="col-sm-4 ">经度(Lat)</p>
-                            <div class="col-sm-8">
-                                <p>{{@$poi->lat}}</p>
-                            </div>
-                        </div>
-                        <div class="col-sm-6" style="padding-left: 0">
-                            <p class="col-sm-4 ">纬度(Lng)</p>
-                            <div class="col-sm-8">
-                                <p>{{@$poi->lng}}</p>
-                            </div>
-                        </div>
-                    </div>
-                    <!--名称-->
-                    <div class="col-md-12 col-sm-12">
-                        <p class="col-sm-2 ">名称(Name)</p>
-                        <div class="col-sm-10">
-                            <p>{{@$poi->poi_name}}</p>
-                        </div>
-                    </div>
-                    <!--地址(Address)-->
-                    <div class="col-md-12 col-sm-12">
-                        <p class="col-sm-2 " >地址(Address)</p>
-                        <div class="col-sm-10">
-                            <p>{{@$poi->address}}</p>
-                        </div>
-                    </div>
+                </div>
+                <div class="poi-detail clearfix">
+                    <h4><span>基本介绍</span></h4>
                     <!--描述-->
                     <div class="col-md-12 col-sm-12">
-                        <p class="col-sm-2">描述(Description)</p>
                         <div class="col-sm-10">
                             <p>{!! @$poi->description !!}</p>
                         </div>
                     </div>
-                </div>
-                <div class="panel-footer">
-                    <a href="{{ url('/travel') }}">返回首页</a>
                 </div>
             </div>
         </div>
@@ -62,20 +80,6 @@
 
 @endsection
 
-@section('css')
-    <link rel="stylesheet" href="{{ asset('/css/travel/main.css') }}">
-    <link rel="stylesheet" href="http://api.map.baidu.com/library/SearchInfoWindow/1.5/src/SearchInfoWindow_min.css" />
-    <style>
-        @media (min-width: 768px){
-            .form-horizontal .control-label {
-                text-align: left;
-                margin-bottom: 0;
-                padding-top: 7px;
-            }
-        }
-
-    </style>
-@endsection
 
 @section('script')
 
