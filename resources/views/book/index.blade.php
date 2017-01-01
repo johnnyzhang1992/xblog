f71aaa52395bedc5d7669627aad669477fbe81d9
johnnyzhang <1113638674@qq.com>
2017-01-02 00:18:49 +0800

    书单列表展示首页-微调

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -2,11 +2,9 @@
 @section('title','书籍')
 @section('content')
     <div class="container">
-
-        <div class="dot"></div>
         <div class="row">
             <div class="col-md-8">
-                <div class="posts-count">共{{ $books_count }}篇文章</div>
+                <div class="posts-count">共{{ @$books_count }}本书</div>
                 <div id="cd-timeline" class="cd-container" style="margin: 0 0">
                     @foreach($books as $book)
                         <div class="cd-timeline-block">
@@ -14,13 +12,14 @@
                             </div>
                             <div class="cd-timeline-content">
                                 <a href="#">
-                                    <div class="title">{{ $book->book_name }}</div>
+                                    <div class="title">{{ @$book->book_name }}<br> <small style="font-size: 70%">作者： {{ @$book->book_author }}</small></div>
                                 </a>
-                                <span class="cd-date">{{ $book->created_at}}</span>
+                                <span class="cd-date">{{ @$book->created_at}}</span>
                             </div>
                         </div>
                     @endforeach
                 </div>
+                <div class="dot"></div>
             </div>
             <div class="col-md-4">
                 <div class="slide">
