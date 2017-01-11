9d10e193e76e0c6f1ff38547ac801f3aa6f6f01d
johnnyzhang <1113638674@qq.com>
2017-01-12 00:34:48 +0800

    书单详情页-007

--- resources/views/book/detail.blade.php
+++ resources/views/book/detail.blade.php
@@ -47,7 +47,6 @@
     </style>
 @endsection
 @section('content')
-
     <div class="container" >
         {{--导航--}}
         <ol class="breadcrumb col-md-10 col-md-offset-1 col-xs-12">
@@ -67,9 +66,7 @@
                     <h4 class="book-name"><span>{{ @$book->book_name }}</span></h4>
                     <div class="col-md-8 col-xs-12">
                         <div class="book_cover_image">
-                            @if(isset($book->cover_image))
-                                <img src='{{ asset(@$book->cover_image) }}' alt="{{ @$book->book_name."封面" }}"  class="img-responsive" >
-                            @endif
+                            <img id="cover_image" src="{{ asset(@$book->cover_image) }}" alt="{{ @$book->book_name."封面" }}"  class="img-responsive" >
                         </div>
                         <div class="book-info">
                             <p class="p1">
@@ -81,7 +78,7 @@
                                 <span class="pl"> 译者</span>:
                                 <span class="book-translator">付强</span>
                             </p>
-                            <p class="pl">出版年: <span class="pubdate">2015-6-1</span></p>
+                            <p class="pl">出版年: <span class="book-pubdate">2015-6-1</span></p>
                             <p class="pl">页数: <span class="book-pages">280</span></p>
                             <p class="pl">ISBN: <span class="book-isbn">9787550013247</span></p>
                         </div>
@@ -98,19 +95,9 @@
                 </div>
                 <div class="book-detail col-md-12 col-xs-12 clearfix">
                     <h4><span>内容简介</span> &nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·</h4>
-                    <div class="book-detail-content">
-                        单亲女孩迪伦，15岁的世界一片狼藉：与母亲总是无话可说，在学校里经常受到同学的捉弄，唯一谈得来的好友也因为转学离开了。这一切都让迪伦感到无比痛苦。
-                        她决定去看望久未谋面的父亲，然而，路上突发交通事故。等她拼命爬出火车残骸之后，却惊恐地发现，自己是唯一的幸存者，而眼前，竟是一片荒原。
-                        此时，迪伦看到不远处的山坡上一个男孩的身影。
-                        男孩将她带离了事故现场。但是，迪伦很快意识到，男孩并不是偶然出现的路人，他似乎是特意在此等候。
-                        命运，从他们相遇的那刻开始，发生了无法预料的转变……
-                    </div>
+                    <div id="book-summary" class="book-detail-content"></div>
                     <h4><span>作者简介</span> &nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·</h4>
-                    <div class="book-detail-content">
-                        克莱儿•麦克福尔居住在苏格兰格拉斯哥南部，是英国文坛备受瞩目的实力作家。
-                        她的作品往往在出其不意的情节架构中饱含感人至深的真情，贯穿着人生思索和人性独白。
-                        《摆渡人》是她最著名的作品，一举摘得五项世界文学大奖，版权销售33个国家，是令千万读者灵魂震颤的心灵治愈小说。
-                    </div>
+                    <div id="author_intro" class="book-detail-content"></div>
                     <h4><span>读书杂记</span> &nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·&nbsp;·</h4>
                     <!--描述-->
                     <div class="book-detail-content">
@@ -150,37 +137,36 @@
 @endsection
 
 @section('script')
+    <script> window.douban_id = '{{ @$book->douban_id }}'</script>
     <script>
         //使用豆瓣API拿指定iD書籍的信息
-        $.ajax({
-            type: "get",
-            url: "https://api.douban.com/v2/book/26356948",//最後一位是書籍id
-            data: $(this).serialize(),
-            async: false,
-            dataType:'jsonp',
-            jsonp: 'callback',
-            success: function(data){
-                console.info(JSON.stringify("id:"+data.id));
-                console.info(JSON.stringify("alt:"+data.alt));
-                console.info(JSON.stringify("alt_title:"+data.alt_title));
-                console.info(JSON.stringify("title:"+data.title));
-                console.info(JSON.stringify("原作名origin_title:"+data.origin_title));
-                console.info(JSON.stringify("cover_image:"+data.image));
-                console.info(JSON.stringify("author:"+data.author_intro));
-                console.info(JSON.stringify("author_intro:"+data.author));
-                console.info(JSON.stringify("简介summary:"+data.summary));
-                console.info(JSON.stringify("序言catalog:"+data.catalog));
-                console.info(JSON.stringify("pages:"+data.pages));
-                console.info(JSON.stringify("ISBN:"+data.isbn13));
-                console.info(JSON.stringify("出版社:"+data.publisher));
-                console.info(JSON.stringify("出版时间:"+data.pubdate));
-                console.info(JSON.stringify("translator:"+data.translator));
-                console.info(JSON.stringify("评分:"+data.rating.average));
-            },
-            error: function (xhr,status,error) {
-                console.info('');
-            }
-        });
+//        var douban_id = window.douban_id;
+//        $.ajax({
+//            type: "get",
+//            url: "https://api.douban.com/v2/book/"+douban_id,//最後一位是書籍id
+//            data: $(this).serialize(),
+//            async: false,
+//            dataType:'jsonp',
+//            jsonp: 'callback',
+//            success: function(data){
+//                $('#cover_image').attr('src',data.image);
+//                $('.book-author').html(data.author);
+//                $('.book-douban-url').attr('href',data.alt);
+//                $('.origin-title').html(data.origin_title);
+//                $('.book-pages').html(data.pages);
+//                $('.book-isbn').html(data.isbn13);
+//                $('.book-publisher').html(data.publisher);
+//                $('.book-pubdate').html(data.pubdate);
+//                $('.book-translator').html(data.translator);
+//                $('.book-douban-rating').html(data.rating.average);
+//                $('#author_intro').html(data.author_intro);
+//                $('#book-summary').html(data.summary);
+////                console.info(JSON.stringify("alt_title:"+data.alt_title));
+//            },
+//            error: function (xhr,status,error) {
+//                console.info('获取豆瓣图书内容失败！');
+//            }
+//        });
     </script>
 
 @endsection
