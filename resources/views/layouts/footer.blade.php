d104adeaa4029cd0a740ebfd939175f847406170
johnnyzhang <1113638674@qq.com>
2018-05-15 18:15:48 +0800

    备案信息

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,7 +5,7 @@
             <div class="row">
                 <div class="col-sm-12">
                     <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
-                    <span> 鲁ICP备15006514号</span> |
+                    <span> 鲁ICP备15006514号-3</span> |
                     <span><a href="https://github.com/johnnyzhang1992/xblog"><i class="fa fa-github fa-fw"></i></a> </span>
                 </div>
             </div>
